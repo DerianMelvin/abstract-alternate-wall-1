34da73c852ee6db69801d56e31da74b1146855d0
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-04-30 14:49:37 +0800

    - Create empty <div> elements - Add id & className to each <div> - Add comments on className & id guides

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+/* 
+  Create background animation
+
+  1. id guide:
+
+      [square-l-1] : Define object based on (shape)-(size)-(number / order)
+
+  2. className guide:
+
+      [container-square-b] : Define groups of shape
+      [square] : Define the shape of container
+      [size-l, size-m, size-s] : Define the size of container
+      [
+        gradient-set-1 : Use gradient color set 1 (black - blue)
+      ]
+*/
 
 function App() {
   return (
     <div className="App">
-
+      <div className="container-square-l">
+        <div id="square-l-1" className="square size-l gradient-set-1"></div>
+        <div id="square-l-2" className="square size-l gradient-set-1"></div>
+        <div id="square-l-3" className="square size-l gradient-set-1"></div>
+      </div>
     </div>
   );
 }
