8b1e2b6d82d897da9a164e4848b2d5cc89ee0921
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-05-15 12:06:22 +0800

    - Create <div> .container-square-m - Create <div> elements in .container-square-m - Add id & className to each <div> in .container-square-m - Adjust guide comments

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 /* 
-  Create background animation
+  TODO: Create background animation
 
-  1. id guide:
-
+  ========================
+    VARIABLE GUIDE
+  ========================
+  1. id:
       [square-l-1] : Define object based on (shape)-(size)-(number / order)
 
-  2. className guide:
-
-      [container-square-b] : Define groups of shape
-      [square] : Define the shape of container
+  2. className:
+      [container-(shape)-(size)] : Define groups of shape & size
+      [(shape)] : Define the shape of container
       [size-l, size-m, size-s] : Define the size of container
       [
-        gradient-set-1 : Use gradient color set 1 (black - blue)
+        theme-set-1 : Use theme color set 1 (black - blue)
       ]
 */
 import './css/main.css'
@@ -20,12 +21,19 @@ function App() {
   return (
     <div className="App">
       <div className="container-square-l">
-        <div id="square-l-1" className="square size-l gradient-set-1"></div>
-        <div id="square-l-2" className="square size-l gradient-set-1"></div>
-        <div id="square-l-3" className="square size-l gradient-set-1"></div>
-        <div id="square-l-4" className="square size-l gradient-set-1"></div>
-        <div id="square-l-5" className="square size-l gradient-set-1"></div>
-        <div id="square-l-6" className="square size-l gradient-set-1"></div>
+        <div id="square-l-1" className="square size-l theme-set-1"></div>
+        <div id="square-l-2" className="square size-l theme-set-1"></div>
+        <div id="square-l-3" className="square size-l theme-set-1"></div>
+        <div id="square-l-4" className="square size-l theme-set-1"></div>
+        <div id="square-l-5" className="square size-l theme-set-1"></div>
+      </div>
+
+      <div className="container-square-m">
+        <div id="square-m-1" className="square size-m theme-set-1"></div>
+        <div id="square-m-2" className="square size-m theme-set-1"></div>
+        <div id="square-m-3" className="square size-m theme-set-1"></div>
+        <div id="square-m-4" className="square size-m theme-set-1"></div>
+        <div id="square-m-5" className="square size-m theme-set-1"></div>
       </div>
     </div>
   );
