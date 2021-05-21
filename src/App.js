9221b810862084d1c4648afdb6ad879e948beda7
DerianM <53027082+DerianMelvin@users.noreply.github.com>
2021-05-21 11:54:21 +0800

    - Adjust 'theme-set-1' className to the following:   > 'theme-color-normal'   > 'theme-color-dark'   > 'theme-color-light' - Adjust guiding comments

--- src/App.js
+++ src/App.js
@@ -11,9 +11,7 @@
       [container-(shape)-(size)] : Define groups of shape & size
       [(shape)] : Define the shape of container
       [size-l, size-m, size-s] : Define the size of container
-      [
-        theme-set-1 : Use theme color set 1 (black - blue)
-      ]
+      [theme-color-normal, theme-color-dark, theme-color-light] : Set default theme color
 */
 import './css/main.css'
 
@@ -21,27 +19,27 @@ function App() {
   return (
     <div className="App">
       <div className="container-square-l">
-        <div id="square-l-1" className="square size-l theme-set-1"></div>
-        <div id="square-l-2" className="square size-l theme-set-1"></div>
-        <div id="square-l-3" className="square size-l theme-set-1"></div>
-        <div id="square-l-4" className="square size-l theme-set-1"></div>
-        <div id="square-l-5" className="square size-l theme-set-1"></div>
+        <div id="square-l-1" className="square size-l theme-color-normal"></div>
+        <div id="square-l-2" className="square size-l theme-color-normal"></div>
+        <div id="square-l-3" className="square size-l theme-color-normal"></div>
+        <div id="square-l-4" className="square size-l theme-color-normal"></div>
+        <div id="square-l-5" className="square size-l theme-color-normal"></div>
       </div>
 
       <div className="container-square-m">
-        <div id="square-m-1" className="square size-m theme-set-1"></div>
-        <div id="square-m-2" className="square size-m theme-set-1"></div>
-        <div id="square-m-3" className="square size-m theme-set-1"></div>
-        <div id="square-m-4" className="square size-m theme-set-1"></div>
-        <div id="square-m-5" className="square size-m theme-set-1"></div>
+        <div id="square-m-1" className="square size-m theme-color-dark"></div>
+        <div id="square-m-2" className="square size-m theme-color-dark"></div>
+        <div id="square-m-3" className="square size-m theme-color-dark"></div>
+        <div id="square-m-4" className="square size-m theme-color-dark"></div>
+        <div id="square-m-5" className="square size-m theme-color-dark"></div>
       </div>
 
       <div className="container-square-s">
-        <div id="square-s-1" className="square size-s theme-set-1"></div>
-        <div id="square-s-2" className="square size-s theme-set-1"></div>
-        <div id="square-s-3" className="square size-s theme-set-1"></div>
-        <div id="square-s-4" className="square size-s theme-set-1"></div>
-        <div id="square-s-5" className="square size-s theme-set-1"></div>
+        <div id="square-s-1" className="square size-s theme-color-light"></div>
+        <div id="square-s-2" className="square size-s theme-color-light"></div>
+        <div id="square-s-3" className="square size-s theme-color-light"></div>
+        <div id="square-s-4" className="square size-s theme-color-light"></div>
+        <div id="square-s-5" className="square size-s theme-color-light"></div>
       </div>
     </div>
   );
